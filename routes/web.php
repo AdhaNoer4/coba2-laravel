fc7e6120ab7765cb686b1ab75cbcb95020c81e2d
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2023-06-10 11:41:44 +0700

    03 COBA LARAVEL | Model, Collection & Controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,55 +31,7 @@ Route::get('/about', function () {
 
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Adha Noer H",
-            "body" => "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod cum rerum provident mollitia eveniet delectus? Repellat eum id doloribus amet nam expedita architecto optio laborum quia dicta minima labore obcaecati rerum, natus deserunt deleniti praesentium! Expedita assumenda tenetur, illo sequi repellat atque a laudantium accusamus dolorem. Corporis sint eveniet sit consectetur, consequuntur ullam, ipsam eligendi perferendis, error aliquid excepturi porro atque est aperiam. Vel quisquam debitis dolorem doloremque deserunt unde nam harum nulla vitae. Recusandae doloribus culpa amet! Porro inventore temporibus at. Quod neque, magnam ut eum beatae aliquid non voluptate ex excepturi, dolor labore odit, earum provident amet."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Mochy Fami",
-            "body" => "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod cum rerum provident mollitia eveniet delectus? Repellat eum id doloribus amet nam expedita architecto optio laborum quia dicta minima labore obcaecati rerum, natus deserunt deleniti praesentium! Expedita assumenda tenetur, illo sequi repellat atque a laudantium accusamus dolorem. Corporis sint eveniet sit consectetur, consequuntur ullam, ipsam eligendi perferendis, error aliquid excepturi porro atque est aperiam. Vel quisquam debitis dolorem doloremque deserunt unde nam harum nulla vitae. Recusandae doloribus culpa amet! Porro inventore temporibus at. Quod neque, magnam ut eum beatae aliquid non voluptate ex excepturi, dolor labore odit, earum provident amet."
-        ]
-    ];
-    return view('posts', [
-        'title' => 'Blog',
-        'posts' => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 // halaman single post
-Route::get('blog/{slug}', function ($slug) {
-
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Adha Noer H",
-            "body" => "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod cum rerum provident mollitia eveniet delectus? Repellat eum id doloribus amet nam expedita architecto optio laborum quia dicta minima labore obcaecati rerum, natus deserunt deleniti praesentium! Expedita assumenda tenetur, illo sequi repellat atque a laudantium accusamus dolorem. Corporis sint eveniet sit consectetur, consequuntur ullam, ipsam eligendi perferendis, error aliquid excepturi porro atque est aperiam. Vel quisquam debitis dolorem doloremque deserunt unde nam harum nulla vitae. Recusandae doloribus culpa amet! Porro inventore temporibus at. Quod neque, magnam ut eum beatae aliquid non voluptate ex excepturi, dolor labore odit, earum provident amet."
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Mochy Fami",
-            "body" => "  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic quod cum rerum provident mollitia eveniet delectus? Repellat eum id doloribus amet nam expedita architecto optio laborum quia dicta minima labore obcaecati rerum, natus deserunt deleniti praesentium! Expedita assumenda tenetur, illo sequi repellat atque a laudantium accusamus dolorem. Corporis sint eveniet sit consectetur, consequuntur ullam, ipsam eligendi perferendis, error aliquid excepturi porro atque est aperiam. Vel quisquam debitis dolorem doloremque deserunt unde nam harum nulla vitae. Recusandae doloribus culpa amet! Porro inventore temporibus at. Quod neque, magnam ut eum beatae aliquid non voluptate ex excepturi, dolor labore odit, earum provident amet."
-        ]
-    ];
-
-    $new_post = [];
-
-    foreach ($blog_posts as $post) {
-        if ($post['slug'] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        'title' => "Single Post",
-        'post' => $new_post
-    ]);
-});
+Route::get('blog/{slug}', [PostController::class, 'show']);
