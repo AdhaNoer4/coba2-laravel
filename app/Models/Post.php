2270a7c80625d42964191faf84595e8e5d81aef7
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2023-06-15 11:46:48 +0700

    11 COBA LARAVEL | Searching & Pagination

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,31 @@ class Post extends Model
     protected $guarded = ['id']; //yg gk boleh di isi, sisanya boleh
     protected $with = ['category', 'author']; //untuk eager loading dipindahkan dari controller ke sini.
 
+    public function scopeFilter($query, array $filters)
+    {
+        // menggunakan when function laravel & null coalecing operator milik php pengganti isset()
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->where('title', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%');
+        });
+
+        $query->when($filters['category'] ?? false, function ($query, $category) {
+            return $query->whereHas('category', function ($query) use ($category) {
+                $query->where('slug', $category);
+            });
+        });
+
+        $query->when(
+            $filters['author'] ?? false,
+            fn ($query, $author) =>
+            $query->whereHas(
+                'author',
+                fn ($query) =>
+                $query->where('username', $author)
+            )
+        );
+    }
+
     public function category()
     {
         // hubungkan class Post ke category 
