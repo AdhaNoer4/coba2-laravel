ede8781ff3c8f5678e065b2a4fa3422869916ae9
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2023-06-16 15:20:05 +0700

    12 COBA LARAVEL | View Login & Registration

---
+++ resources/views/register/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.main')
+
+@section('container')
+
+<div class="row justify-content-center">
+    <div class="col-lg-5">
+        <main class="form-registration w-100 m-auto">
+            <h1 class="h3 mb-3 fw-normal text-center">Registration Form</h1>
+            <form>
+             
+          
+              <div class="form-floating">
+                <input type="text" class="form-control rounded-top" id="name" placeholder="Name..." name="name">
+                <label for="name">Name</label>
+              </div>
+              <div class="form-floating">
+                <input type="text" class="form-control" id="username" placeholder="UserName..." name="username">
+                <label for="username">Username</label>
+              </div>
+              <div class="form-floating">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                <label for="floatingInput">Email address</label>
+              </div>
+              <div class="form-floating">
+                <input type="password" name="password" class="form-control rounded-bottom" id="password" placeholder="Password">
+                <label for="password">Password</label>
+              </div>
+          
+             
+              <button class="btn btn-primary w-100 py-2 mt-3" type="submit">Register</button>
+              
+            </form>
+            
+            <small class="d-block text-center mt-3">
+                Already Registered? 
+                <a href="/login" >Login!</a>
+            </small>
+          </main>
+    </div>
+</div>
+    
+
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Post;
+
 use App\Models\Category;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -52,3 +53,10 @@ Route::get('/categories', function () {
 
     ]);
 });
+
+
+// Halaman Login 
+Route::get('/login', [LoginController::class, 'index']);
+
+//Halaman Register
+Route::get('/register', [RegisterController::class, 'index']);
